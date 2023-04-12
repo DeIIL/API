2583f08a4d754991512e2a8e1eb3e73c384ae4db
DeIIL <gudellcastillo@gmail.com>
2023-04-12 19:28:12 -0300

    added import-category-route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRouter = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRouter.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +17,10 @@ categoriesRouter.post('/', (request, response) => {
 
 categoriesRouter.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRouter.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRouter };
