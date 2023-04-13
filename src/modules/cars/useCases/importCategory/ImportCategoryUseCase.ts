0e99dae4c9467bf66087baa769ca57becc21f1a5
DeIIL <gudellcastillo@gmail.com>
2023-04-13 15:22:38 -0300

    added stream to import csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
